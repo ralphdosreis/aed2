6461e8476557ba04d45a6ed501d7f54dac53c239
Ralph dos Reis Torres <ralphdosreistorres@gmail.com>
2026-03-12 20:04:24 -0300

    at1 matriz

--- at1/matriz.c
+++ at1/matriz.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 
-#define Alunos 5
+#define Alunos 30
 
-int main() {
+int main()
+{
     float dados[Alunos][2];
     float somaNotas = 0;
     int aprovados = 0, reprovados = 0;
 
-    for (int i = 0; i < Alunos; i++) {
-        printf("Aluno %d - Nota: ", i+1);
+    for (int i = 0; i < Alunos; i++)
+    {
+        printf("Aluno %d - Nota: ", i + 1);
         scanf("%f", &dados[i][0]);
-        printf("Aluno %d - Frequencia: ", i+1);
+        printf("Aluno %d - Frequencia: ", i + 1);
         scanf("%f", &dados[i][1]);
 
         somaNotas += dados[i][0];
     }
 
-    for (int i = 0; i < Alunos; i++) {
-        if (dados[i][0] >= 6.0 && dados[i][1] >= 75.0) {
-            printf("Aluno %d: Aprovado\n", i+1);
+    for (int i = 0; i < Alunos; i++)
+    {
+        if (dados[i][0] >= 6.0 && dados[i][1] >= 75.0)
+        {
+            printf("Aluno %d: Aprovado\n", i + 1);
             aprovados++;
-        } else {
+        }
+        else
+        {
             reprovados++;
-            if (dados[i][0] < 6.0 && dados[i][1] < 75.0) {
-                printf("Aluno %d: Reprovado por nota e frequência\n", i+1);
-            } else if (dados[i][0] < 6.0) {
-                printf("Aluno %d: Reprovado por nota\n", i+1);
-            } else {
-                printf("Aluno %d: Reprovado por frequência\n", i+1);
-
+            if (dados[i][0] < 6.0 && dados[i][1] < 75.0)
+            {
+                printf("Aluno %d: Reprovado por nota e frequencia\n", i + 1);
+            }
+            else if (dados[i][0] < 6.0)
+            {
+                printf("Aluno %d: Reprovado por nota\n", i + 1);
+            }
+            else
+            {
+                printf("Aluno %d: Reprovado por frequencia\n", i + 1);
             }
         }
     }
 
     printf("\nResultados: \n");
-    printf("Média da turma: %.2f\n", somaNotas / Alunos);
+    printf("Media da turma: %.2f\n", somaNotas / Alunos);
     printf("Total de aprovados: %d\n", aprovados);
     printf("Total de reprovados: %d\n", reprovados);
 }
